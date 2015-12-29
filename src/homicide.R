43665872777e371f4217167bd53fa25d2b09cf59
Diego Valle-Jones <diegovalle@gmail.com>
2015-12-29 13:55:24 -0600

    correlation test

--- src/homicide.R
+++ src/homicide.R
@@ -7,16 +7,15 @@ afros$MUN <- as.integer(afros$MUN)
 corr_chart <- function(years, years_str, filename) {
   hom <- injury.intent %>%
     filter(state_reg %in% c(12,20,30) & intent == "Homicide" & year_reg %in% c(years)) %>%
-    group_by(state_reg, mun_reg) %>%
+    group_by(state_reg, mun_reg, year_reg) %>%
     summarise(homicides = n()) %>%
     left_join(afros, by = c('state_reg' = 'ENT', 'mun_reg' = 'MUN')) %>%
-    mutate(rate = homicides / (total * (max(years) - min(years))) * 10^5) %>%
+    mutate(rate = homicides / (total ) * 10^5) %>%
     mutate(NOM_ENT = str_replace(NOM_ENT, " de Ignacio de la Llave", ""))
-  
-  dfcoef <- hom %>%
-    group_by(NOM_ENT) %>%
-    do(fit = lm(rate ~ per,data = .))
-  print(tidy(dfcoef, fit))
+
+   gp <- group_by(hom, NOM_ENT) %>%
+     do(tidy(cor.test(~ rate + per, data = .)))
+   print(gp)
   
   gg <- ggplot(hom, aes(per, rate, group = NOM_ENT, color = NOM_ENT)) +
     geom_point(alpha = .5) +
@@ -27,6 +26,7 @@ corr_chart <- function(years, years_str, filename) {
     ylab(str_c("homicide rate ", years_str)) +
     ggtitle("Correlations between homicide rates and percentage Afro-Mexican\nat the municipio level, by state")
   ggsave(filename, plot = gg, dpi = 100, width = 9, height = 5)
+  gg
 }
 corr_chart(2008:2014, "2008-2014", "graphs/homicides_drugwar.svg")
 corr_chart(2004:2007, "2004-2007", "graphs/homicides_before.svg")
